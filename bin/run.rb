956c95592528e427249e4f23d2760d2e9b63a484
jbmenashi <jbmenashi@gmail.com>
2018-11-12 15:18:42 -0500

    we did it

--- bin/run.rb
+++ bin/run.rb
@@ -6,3 +6,8 @@ require_relative "../lib/command_line_interface.rb"
 welcome
 character = get_character_from_user
 show_character_movies(character)
+
+
+# welcome
+# title = get_movie_title_from_user
+# movie_by_name(title)
